51a2def7506737c56ee0e3182b7abcd8a722fe13
ChandrakalaKirasur <59432828+ChandrakalaKirasur@users.noreply.github.com>
2020-06-17 03:54:15 +0530

    [ChandrakalaKirasur]commit to check jenkins

---
+++ src/main/java/com/bridgelabz/jenkinstest/JenkinsTestApplication.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.jenkinstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class JenkinsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsTestApplication.class, args);
+	}
+
+}
+@RestController
+class JenkinsTestController {
+	/*
+	 * welcome page
+	 */
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "welcome ";
+	}
+}
